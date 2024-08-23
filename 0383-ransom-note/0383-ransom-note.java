6903c89d33a278095dd0e2319e0ca2bdc4eca7ef
Malarselvimarimuthu <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-08-23 16:09:13 +0530

    Time: 2 ms (79.88%), Space: 44.4 MB (86.23%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) 
+    {
+        int rlen = ransomNote.length();
+        int mlen = magazine.length();
+        int cnt[] = new int[26];
+        if(mlen>=rlen)
+        {
+            for(int i=0;i<mlen;i++)
+            {
+                cnt[magazine.charAt(i)-'a']++;
+            }
+            for(int i=0;i<rlen;i++)
+            {
+                cnt[ransomNote.charAt(i)-'a']--;
+            }
+            int tot = mlen-rlen;
+            int cnt1 = 0;
+            for(int i=0;i<26;i++)
+            {
+                if(cnt[i]>0)
+                    cnt1+=cnt[i];
+            }
+            if(cnt1==tot)
+                return true;
+        }
+        
+        return false;
+    }
+}
