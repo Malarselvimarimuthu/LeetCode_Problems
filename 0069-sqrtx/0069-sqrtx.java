977eb0ffd0bf74e811780260d4f0e9d8df196230
Malarselvimarimuthu <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-12 15:24:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        int mid = 0;
+        while(start<=end)
+        {
+            mid = start + (end - start)/2;
+            if(mid*mid>x)
+                end = mid-1;
+            else if(mid*mid==x)
+                return mid;
+            else
+                start = mid+1;        
+        }
+        return Math.round(end);
+    }
+}
