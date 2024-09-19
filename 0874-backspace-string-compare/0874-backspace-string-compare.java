c2096bd0987fe1a7a833f140a68cbe44dab9e3bf
Malarselvi M <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-19 10:21:37 +0530

    Time: 2 ms (56.74%), Space: 41.8 MB (28.71%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -2,14 +2,13 @@ class Solution {
     public boolean backspaceCompare(String s, String t) {
         s = compare(s);
         t = compare(t);
-        System.out.println(s);
-        System.out.println(t);
+        // System.out.println(s);
+        // System.out.println(t);
         return s.equals(t);
     }
     public String compare(String s)
     {
         Stack<Character> stack = new Stack<>();
-        String str="";
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='#')
@@ -24,9 +23,6 @@ class Solution {
                 stack.push(s.charAt(i));
             }
         }
-        while(!stack.isEmpty()) {
-            str +=stack.pop();
-        }
-        return str;
+        return String.valueOf(stack);
     }
 }
