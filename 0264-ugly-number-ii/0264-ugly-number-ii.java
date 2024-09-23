6c9ae145de0a747b8e629a8b853c8b3466e3b4a9
Malarselvi M <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-23 13:21:43 +0530

    Time: 2 ms (96.55%), Space: 41.7 MB (65.57%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int nthUglyNumber(int n) 
+    {
+        int ugly = 1;
+        int arr[] = new int[n];
+        int pos1 = 0;
+        int pos2 = 0;
+        int pos3 = 0;
+        arr[0] = 1;
+        int min = 1;
+        for(int i=1;i<n;i++)
+        {
+            min = Math.min(arr[pos1]*2,Math.min(arr[pos2]*3,arr[pos3]*5));
+            if(min == arr[pos1]*2)pos1++;
+            if(min == arr[pos2]*3)pos2++;
+            if(min == arr[pos3]*5)pos3++;
+            arr[i] = min;
+        }   
+        return arr[n-1];
+    }
+}
