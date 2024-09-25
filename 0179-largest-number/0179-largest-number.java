ee82683944ea1ab2592749731fcf14a35b09ed53
Malarselvi M <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-25 10:53:55 +0530

    Time: 6 ms (52.35%), Space: 42.5 MB (96.6%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -6,18 +6,17 @@ class Solution {
       {
         list.add(String.valueOf(nums[i]));
       }   
+      
       Collections.sort(list,(a,b)->(b+a).compareTo(a+b));
       if (list.get(0).equals("0")) {
             return "0";
         }
-
-        // Build the final largest number by joining the sorted strings
+       
         StringBuilder result = new StringBuilder();
         for (String s : list) {
             result.append(s);
         }
 
-        // Return the result as a string
         return result.toString();
     }
 }
