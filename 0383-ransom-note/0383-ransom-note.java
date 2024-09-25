8f78a3ec9050fb6d7c8bb879c4ae203a97e6a85b
Malarselvi M <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-25 15:33:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -12,17 +12,19 @@ class Solution {
             }
             for(int i=0;i<rlen;i++)
             {
+                if(cnt[ransomNote.charAt(i) - 'a']==0)
+                    return false;
                 cnt[ransomNote.charAt(i)-'a']--;
             }
-            int tot = mlen-rlen;
-            int cnt1 = 0;
-            for(int i=0;i<26;i++)
-            {
-                if(cnt[i]>0)
-                    cnt1+=cnt[i];
-            }
-            if(cnt1==tot)
-                return true;
+            // int tot = mlen-rlen;
+            // int cnt1 = 0;
+            // for(int i=0;i<26;i++)
+            // {
+            //     if(cnt[i]>0)
+            //         cnt1+=cnt[i];
+            // }
+            // if(cnt1==tot)
+            //     return true;
         }
         
         return false;
