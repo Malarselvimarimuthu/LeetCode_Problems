224f4b078c922f737252a1dddbfe57ddf9b91de1
Malarselvi M <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-25 15:47:14 +0530

    Time: 10 ms (58.32%), Space: 44.9 MB (57.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,21 +1,18 @@
 class Solution {
-    public boolean canConstruct(String r, String m) 
-    {
-        int a[]=new int[26];
-        for(int i=0;i<r.length();i++)
-        {
-            a[r.charAt(i)-'a']++;
-        }
-         for(int i=0;i<m.length();i++)
-        {
-            a[m.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++)
-        {
-            if(a[i]>0)
-            return false;
-        }
-        return true;
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magaHash = new HashMap<>();
 
+        for (char c : magazine.toCharArray()) {
+            magaHash.put(c, magaHash.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!magaHash.containsKey(c) || magaHash.get(c) <= 0) {
+                return false;
+            }
+            magaHash.put(c, magaHash.get(c) - 1);
+        }
+
+        return true;
     }
 }
