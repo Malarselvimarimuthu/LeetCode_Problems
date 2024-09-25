8f38f58cb1fcc990ca408aceb849f695b49e29b6
Malarselvi M <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-09-25 15:43:08 +0530

    Time: 1 ms (99.14%), Space: 44.5 MB (78.95%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,32 +1,21 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) 
+    public boolean canConstruct(String r, String m) 
     {
-        int rlen = ransomNote.length();
-        int mlen = magazine.length();
-        int cnt[] = new int[26];
-        if(mlen>=rlen)
+        int a[]=new int[26];
+        for(int i=0;i<r.length();i++)
         {
-            for(int i=0;i<mlen;i++)
-            {
-                cnt[magazine.charAt(i)-'a']++;
-            }
-            for(int i=0;i<rlen;i++)
-            {
-                if(cnt[ransomNote.charAt(i) - 'a']==0)
-                    return false;
-                cnt[ransomNote.charAt(i)-'a']--;
-            }
-            // int tot = mlen-rlen;
-            // int cnt1 = 0;
-            // for(int i=0;i<26;i++)
-            // {
-            //     if(cnt[i]>0)
-            //         cnt1+=cnt[i];
-            // }
-            // if(cnt1==tot)
-            //     return true;
+            a[r.charAt(i)-'a']++;
         }
-        
-        return false;
+         for(int i=0;i<m.length();i++)
+        {
+            a[m.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]>0)
+            return false;
+        }
+        return true;
+
     }
 }
