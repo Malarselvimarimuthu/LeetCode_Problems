c15d3c8fff413879a0215ecb207bff9b2d0b80a8
Malarselvi M <malarselvim.22cse@kongu.edu>
2025-03-29 22:03:09 +0530

    Time: 345 ms (94.86%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT name FROM Employee WHERE id IN 
-(SELECT managerId 
-FROM Employee 
-GROUP BY managerId
-HAVING count(managerId)>=5);
+SELECT a.name 
+FROM Employee a JOIN Employee b
+ON a.id = b.managerId
+GROUP BY a.id
+HAVING COUNT(b.managerId)>=5;
