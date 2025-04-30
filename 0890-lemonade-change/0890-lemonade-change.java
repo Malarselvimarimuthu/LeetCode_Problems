856d774cd9fe6ae92a9bbc5868e619270b365b48
Malarselvi M <malarselvim.22cse@kongu.edu>
2025-04-30 14:03:59 +0530

    Time: 2 ms (94.05%), Space: 55.6 MB (86.17%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -4,7 +4,7 @@ class Solution {
         int n = bills.length;
         int cnt5 = 0;
         int cnt10 = 0;
-        int cnt20 = 0;
+        
         for(int i=0;i<n;i++){
             if(bills[i] == 5){
                 cnt5 ++;
@@ -25,11 +25,11 @@ class Solution {
                 if(cnt10>=1 && cnt5>=1){
                     cnt10 --;
                     cnt5--;
-                    cnt20++;
+                   
                 }
                 else if(cnt5>=3){
                     cnt5 = cnt5 - 3;
-                    cnt20 ++;
+                    
                 }
                 else{
                     return false;
