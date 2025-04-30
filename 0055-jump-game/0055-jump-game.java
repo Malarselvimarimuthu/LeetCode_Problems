f4d2ff2e40bb250353f52a7eb547d67ca5c20e84
Malarselvi M <malarselvim.22cse@kongu.edu>
2025-04-30 10:21:37 +0530

    Time: 1623 ms (5.01%), Space: 46.1 MB (13.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,22 @@
+import java.util.*;
 class Solution {
     public boolean canJump(int[] nums) {
-       int reachable = 0;
-       for(int i = 0; i < nums.length; i ++) {
-           if(i > reachable) return false;
-           reachable = Math.max(reachable, i + nums[i]);
-       } 
-       return true;
+        int len = nums.length;
+        int dp[] = new int[len];
+        Arrays.fill(dp,len);
+        return maxJump(nums,dp);
+    }
+    public boolean maxJump(int[] nums,int[] dp)
+    {
+        dp[0] = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=1;j<=nums[i];j++){
+                if(i+j<nums.length){
+                dp[i+j] = Math.min(dp[i+j],dp[i]+1);
+                }
+            }
+        }
+        if(dp[nums.length-1]==nums.length) return false;
+        return true;
     }
 }
