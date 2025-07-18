47e2b3471c78928a4b848d6cef5e049b13dc09c7
Malarselvi M <malarselvim.22cse@kongu.edu>
2025-07-18 11:36:41 +0530

    Time: 0 ms (100%), Space: 41.3 MB (34.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,42 @@
 class Solution {
-    public int rob(int[] nums) {
-        
-        // Tabulization
-        int n = nums.length;
-
-        int dp[] = new int[n];
-        dp[0] = nums[0];
-        for(int i=1;i<n;i++)
-        {
-           
-            int pick = nums[i] ;
-            if(i>1)
-            {
-                pick += dp[i-2];
-            }
-            int notPick = dp[i-1];
-
-            dp[i] = Math.max(pick,notPick);
-        }
-        return dp[n-1];
+    public int rob(int[] nums) 
+    {
+       int n = nums.length-1; 
+       int dp[] = new int[n+1];
+       Arrays.fill(dp,-1);
+       return helper(n,nums,dp);
     }
+
+    public int helper(int ind,int[] nums,int[] dp)
+    {
+        if(ind == 0)
+        {
+            return nums[ind];
+        }
+        if(ind < 0)
+        {
+            return 0;
+        }
+        if(dp[ind] != -1) return dp[ind];
+        int pick = nums[ind] + helper(ind-2,nums,dp); 
+        int notPick = helper(ind-1,nums,dp);
+
+        return dp[ind] = Math.max(pick,notPick); 
+    }
+
+    // dp [0,0,0,0,0]
+    // [1,2,3,1]
+    // if(ind == 0)
+    // {
+    //     return nums[ind];
+    // }
+    // if(ind <0)
+    // {
+    //     return 0;
+    // }
+    // int pick += helper(nums,ind-1);
+    // int notPick += helper(nums, ind);
+    // // Math.max(pick,)
+    // return Math.max(pick,notPick);
+
 }
