4e2367492e315e048338708f065ce3bd65e4bf93
Malarselvi M <malarselvim.22cse@kongu.edu>
2025-07-22 08:59:06 +0530

    Time: 7 ms (34.54%), Space: 43.7 MB (89.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,31 @@
+import java.util.HashMap;
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
-        int max = 0;
-        for(int right=0;right<s.length();right++)
+    public int lengthOfLongestSubstring(String s) 
+    {
+        if(s==null)
         {
-            char ch = s.charAt(right);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            while(map.get(ch)>1)
-            {
-                char leftChar = s.charAt(left);
-                map.put(leftChar,map.get(leftChar)-1);
-                left++;
-            }
-            max = Math.max(max,right-left+1);
+            return 0;
         }
+        HashMap<Character,Integer> map = new HashMap<>();
+        int left = 0;
+        int max = 0;
+        for(int i=0;i<s.length();i++) 
+        {
+           char ch = s.charAt(i);
+           int count = map.getOrDefault(ch, 0);
+
+            map.put(ch, count + 1);
+
+                while( map.get(ch) >1)  
+                {
+                    char leftChar = s.charAt(left);
+                    map.put(leftChar,map.get(leftChar)-1);
+                    left++;
+                }
+            
+            max = Math.max(i-left+1,max);
+        }
+        
         return max;
     }
 }
